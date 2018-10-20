dc578cc6c0dde383ca5a7433bbc765c4e5442165
Arthur Vernet <arthurvernet@gmail.com>
2018-10-20 11:53:47 +0200

    implementations.py done

--- costs.py
+++ costs.py
@@ -1,20 +1,28 @@
-# -*- coding: utf-8 -*-
-"""Function used to compute the loss."""
 import numpy as np
 from proj1_helpers import *
-def compute_loss(y, tx, w):
-    """Calculate the loss.
 
-    You can calculate the loss using mse or mae.
-    """
-    e=y-tx@np.transpose(w)
-    return (1/(2*len(y)))*sum(e**2)
 def accuracy(y, tx, w):
-    
-    """Calculate the loss.
-
-    You can calculate the loss using mse or mae.
-    """
     pred=predict_labels(w,tx)
     e=y-pred
     return (1/(2*len(y)))*sum(e**2)
+
+def compute_mse_loss(y, tx, w):
+    """Compute the mean square error."""
+    return compute_mse(compute_error(y, tx, w))
+
+def compute_mae_loss(y, tx, w):
+    """Compute the mean absolute error."""
+    return compute_mae(compute_error(y, tx, w))
+
+def compute_error(y, tx, w):
+    """Compute the error"""
+    e = y-tx@w.T
+    return e
+
+def compute_mse(e):
+    loss = np.mean(e**2)/2
+    return loss
+
+def compute_mae(e):
+    loss = np.mean(abs(e))/2
+    return loss
---
+++ implementations.py
@@ -0,0 +1,52 @@
+import numpy as np
+from costs import *
+from gradient_descent import compute_gradient
+from helpers import batch_iter
+
+def least_squares_GD(y, tx, initial_w, max_iters, gamma):
+    """Linear regression using gradient descent."""
+    w = initial_w
+    for n_iter in range(max_iters):
+        grad, e = compute_gradient(y, tx, w)
+        w = w - gamma*grad
+    loss = compute_mse(e)
+    return (w, loss)
+
+def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
+    """Linear regression using stochastic gradient descent."""
+    batch_size = 1 #as requested in the project desription
+    w = initial_w
+    for n_iter in range(max_iters):
+        for y_batch, tx_batch in batch_iter(y, tx, batch_size):
+            grad, e = compute_gradient(y_batch, tx_batch, w)
+            w = w - gamma*grad
+    loss = compute_mse(e)
+    return (w, loss)
+
+def least_squares(y, tx):
+    """Least squares regression using normal equations."""
+    a = tx.T@tx #Gram matrix: X_T*X
+    b = tx.T@y #X_T*y
+    w = np.linalg.solve(a, b) #solve (X_T*X)*w = X_T*y and return w
+    loss = compute_mse_loss(y, tx, w)
+    return (w, loss)
+
+def ridge_regression(y, tx, lambda_):
+    """Ridge regression using normal equations."""
+    N = y.shape[0]
+    lambda_I = 2*N*lambda_ *np.identity(tx.shape[1])
+    a = tx.T@tx + lambda_I
+    b = tx.T@y
+    w = np.linalg.solve(a, b) #solve (X_T*X + 2*N*lambda*I)*w = X_T*y and return w
+    loss = compute_mse_loss(y, tx, w)
+    return (w, loss)
+
+def logistic_regression(y, tx, initial_w, max_iters, gamma):
+    """Logistic regression using gradient descent or SGD."""
+    raise NotImplementedError
+    return (w, loss)
+
+def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
+    """Regularized logistic regression using gradient descent or SGD."""
+    raise NotImplementedError
+    return (w, loss)
