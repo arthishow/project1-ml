14d8030dd1738fb28387da7242df9ecbc225acff
Arthur Vernet <arthurvernet@gmail.com>
2018-10-20 12:05:21 +0200

    bit of cleaning

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # CS-433 Machine Learning - Project 1
 - Robin Berguerand
 - Arthur Vernet
+- Pablo Guilland
 
 [Report link](https://www.overleaf.com/8274881383mhvqpvfgcrbk)
--- helpers.py
+++ helpers.py
@@ -40,3 +40,21 @@ def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
         end_index = min((batch_num + 1) * batch_size, data_size)
         if start_index != end_index:
             yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
+
+def split_data(x, y, ratio, seed=1):
+    """
+    split the dataset based on the split ratio. If ratio is 0.8
+    you will have 80% of your data set dedicated to training
+    and the rest dedicated to testing
+    """
+    # set seed
+    n_sample=int(np.floor(ratio*len(x)))
+    x_copy=np.copy(x)
+    y_copy=np.copy(y)
+    np.random.seed(seed)
+    np.random.shuffle(x_copy)
+    x_train,x_test=np.split(x_copy, [n_sample])
+    np.random.seed(seed)
+    np.random.shuffle(y_copy)
+    y_train,y_test=np.split(y_copy, [n_sample])
+    return x_train,x_test,y_train,y_test
