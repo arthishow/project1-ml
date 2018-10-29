adc8b4224b55fac4abbb7bbdef2629bc0bc4e77d
Arthur Vernet <arthurvernet@gmail.com>
2018-10-29 21:17:00 +0100

    small change

--- run.py
+++ run.py
@@ -2,7 +2,6 @@ from proj1_helpers import *
 from helper import preprocess_datasets, generate_prediction
 
 DATA_PATH = 'data/'
-PREDICTION_PATH = 'predictions/'
 y_tr, x_tr, ids_tr = load_csv_data(DATA_PATH + "train.csv")
 y_te, x_te, ids_te = load_csv_data(DATA_PATH + "test.csv")
 
@@ -10,4 +9,4 @@ x_tr_0, y_tr_0, x_tr_1, y_tr_1, x_tr_2, y_tr_2, x_tr_3, y_tr_3, x_te_0, x_te_1, 
 
 predicted_y_te = generate_prediction(x_tr_0, y_tr_0, x_tr_1, y_tr_1, x_tr_2, y_tr_2, x_tr_3, y_tr_3, x_te_0, x_te_1, x_te_2, x_te_3, jet_num_te)
 
-create_csv_submission(ids_te, predicted_y_te, PREDICTION_PATH + "output.csv")
+create_csv_submission(ids_te, predicted_y_te, "output.csv")
